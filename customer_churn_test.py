34e0814e694b719af04c30d987a4c7b29218a768
Archana G S <62071221+archanags001@users.noreply.github.com>
2025-03-14 16:40:48 -0700

    Update customer_churn_test.py

--- customer_churn_test.py
+++ customer_churn_test.py
@@ -61,7 +61,8 @@ if __name__ == "__main__":
     # st.title("Churn Prediction Dashboard")
     data = load_data()
     X_train, X_test, y_train, y_test, feature_names = process_data(data)
-    log_reg = joblib.load("https://github.com/archanags001/CustomerChurnPredictor/blob/main/customer_churn_model.pkl")
+    # log_reg = joblib.load("https://github.com/archanags001/CustomerChurnPredictor/blob/main/customer_churn_model.pkl")
+    log_reg = joblib.load('https://github.com/archanags001/CustomerChurnPredictor/blob/main/customer_churn_model.pkl#:~:text=customer_churn_model.pkl')
 
     col1, col2 = st.columns([1,2])  # Left, Center, Right
 
