762f5ba1ef1b3ac2e8460e29927f0ecebc82e4d9
Archana G S <62071221+archanags001@users.noreply.github.com>
2025-03-14 16:11:00 -0700

    Add files via upload

---
+++ customer_churn_test.py
@@ -0,0 +1,233 @@
+from customer_churn_training import *
+import shap
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+def display_feature_insights(model, X_test, feature_names):
+    col_a, col_b = st.columns([1, 1])
+    with col_a:
+        st.markdown("<h3 class='centered-header'>Feature Impact on Customer Churn Predictions</h3>", unsafe_allow_html=True)
+        X_test_scaled = model.named_steps['scaler'].transform(X_test)
+        explainer = shap.Explainer(model.named_steps['classifier'], X_test_scaled, feature_names=feature_names)
+        shap_values = explainer(X_test_scaled)
+        shap.summary_plot(shap_values, X_test_scaled, feature_names=feature_names,max_display=12, show=False)
+        st.pyplot(plt.gcf())
+        plt.clf()
+    with col_b:
+        st.markdown("<h3 class='centered-header'>Feature for Dependence Plot</h3>", unsafe_allow_html=True)
+        st.write("")
+        st.write("")
+        feature_index = st.selectbox("Select Feature for Dependence Plot", feature_names)
+        st.write("")
+        st.write("")
+        feature_col_index = feature_names.tolist().index(feature_index)
+        shap.dependence_plot(feature_col_index, shap_values.values, X_test_scaled, feature_names=feature_names, show=False)
+        st.pyplot(plt.gcf())
+        plt.clf()
+
+# Plots feature importance for a Logistic Regression model using Plotly.
+def plot_feature_importance_plotly(model, feature_names):
+    importance = pd.DataFrame({
+        'Feature': feature_names,
+        'Importance': model.named_steps['classifier'].coef_[0]
+    })
+    importance = importance.sort_values(by='Importance', ascending=False)
+
+    fig = px.bar(
+        importance,
+        x='Feature',
+        y='Importance',
+        title="Feature Importance (Logistic Regression)",
+        labels={'Importance': 'Coefficient Value'},
+    )
+    fig.update_layout(xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+if __name__ == "__main__":
+    st.set_page_config(layout='wide')
+    st.markdown(
+        """
+        <style>
+        .centered-header {
+            text-align: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.markdown("<h1 style='text-align: center;'>Churn Prediction Dashboard</h1>", unsafe_allow_html=True)
+    # st.title("Churn Prediction Dashboard")
+    data = load_data()
+    X_train, X_test, y_train, y_test, feature_names = process_data(data)
+    log_reg = joblib.load("customer_churn_model.pkl")
+
+    col1, col2 = st.columns([1,2])  # Left, Center, Right
+
+    with col1:
+        st.subheader("📊 Model Performance")
+        with st.container():
+            evaluate_models(log_reg, X_test, y_test)
+
+            plot_feature_importance_plotly(log_reg, X_train.columns)
+
+    with col2:
+        st.subheader("🔍 Select Test Data Option")
+        option = st.radio(
+            "Choose an option:",
+            ["Edit/Add Data Manually", "Upload Data File", "Use Existing Data"]
+        )
+
+        if option == "Upload Data File":
+            # Allow the user to upload CSV or XLSX files
+            uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx"])
+
+            # Process the uploaded file if a file is uploaded
+            if uploaded_file is not None:
+                try:
+                    if uploaded_file.name.endswith('.csv'):
+                        try:
+                            test_data = pd.read_csv(uploaded_file)
+                        except Exception as e:
+                            st.error(str(e))
+                    elif uploaded_file.name.endswith(('.xlsx', '.xls')):
+                        try:
+                            test_data = pd.read_excel(uploaded_file)
+
+                        except Exception as e:
+                            st.error(str(e))
+                    # Ensure test_data has the same columns as data used before training
+                    try:
+                        col_names = list(data.columns)
+                        col_names.remove('Churn')
+                        test_data = test_data[col_names]
+                    except Exception as e:
+                        st.error(e)
+                        st.info('Take a moment to inspect the test data features.')
+
+                    if len(test_data) < 1:
+                        st.warning("Test data is empty! Please add at least one row before running predictions.")
+                    else:
+                        st.dataframe(test_data)
+                        st.session_state.test_data = test_data
+
+                except Exception as e:
+                    st.error(f"Error reading file: {e}")
+
+        elif option == "Edit/Add Data Manually":
+
+            drop_down_columns = ['gender','Partner', 'Dependents', 'PhoneService', 'MultipleLines', 'InternetService',
+                                   'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
+                                   'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod', 'Churn']
+            test_data = pd.DataFrame()
+            churn_0 = data[data['Churn'] == 'No']
+            churn_1 = data[data['Churn'] == 'Yes']
+            sample_data = pd.concat([churn_0[:4], churn_1[:4]], ignore_index=True)
+
+            # Convert categorical columns to categorical type
+            for col in data.columns:
+                if (col in drop_down_columns) and (col not in ['customerID','Churn']) :
+                    test_data[col] = sample_data[col].astype(pd.CategoricalDtype(data[col].unique()))
+                elif (col not in ['customerID','Churn']) :
+                    test_data[col] = sample_data[col]
+
+            test_data['SeniorCitizen'] = test_data['SeniorCitizen'].map({'No':0, 'Yes':1})
+
+
+            # Create a form to control updates
+            with st.form(key="edit_form"):
+                edited_data = st.data_editor(test_data, num_rows='dynamic', use_container_width=True)
+                # edited_data = st.data_editor(st.session_state.test_data, num_rows='dynamic', use_container_width=True,
+                #                              key="editor")
+
+                # Submit button inside the form
+                submit_button = st.form_submit_button("Submit Changes")
+
+            # Only update session state when the form is submitted
+            if submit_button:
+                if len(edited_data) < 1:
+                    st.warning("Test data is empty! Please add at least one row before running predictions.")
+                else:
+                    st.session_state.test_data = edited_data
+                    st.success("Test data updated successfully!")
+
+        else:
+            test_data = pd.read_csv('Test_data_no_churn.csv')
+
+            if st.button("Submit Test Data"):
+                st.dataframe(test_data)
+                st.session_state.test_data = test_data
+
+    # Retrieve df from session state
+    if 'test_data' in st.session_state:
+        test_data = st.session_state.test_data
+        # copy_test_data = test_data.copy()
+        test_data_processed = (test_data
+                        .pipe(process_senior_citizen)
+                        .pipe(process_total_charges)
+                        .pipe(encode_categorical)
+                        )
+        test_data_processed = test_data_processed.reindex(columns=feature_names, fill_value=0)
+        # Predict with the trained model
+        X_test_new = log_reg.named_steps['scaler'].transform(test_data_processed)
+        y_pred_prob = log_reg.named_steps['classifier'].predict_proba(X_test_new)[:,
+                      1]  # Prediction probabilities (probability of class 1)
+        y_pred = (y_pred_prob >= 0.5).astype(int)  # Convert probabilities to binary predictions
+        y_pred = (y_pred >= 0.5).astype(int)
+
+        # Add prediction probabilities and churn predictions to the original test_data
+        test_data['Churn_Probability'] = y_pred_prob
+        test_data['Churn_Prediction'] = y_pred
+
+
+        # Function to apply row-based coloring (red for churn, green for no churn)
+        def highlight_churn(row):
+            color = "background-color: red; color: white;" if row[
+                                                                  "Churn_Prediction"] == 1 else "background-color: lightgreen; color: black;"
+            return [color] * len(row)
+
+
+        # Apply the styling to the original test_data
+        styled_df = test_data.style.apply(highlight_churn, axis=1)
+
+        st.markdown("<h3 class='centered-header'> Test Data with Predictions and Probabilities</h3>", unsafe_allow_html=True)
+
+        st.dataframe(styled_df, use_container_width=True)
+
+        display_feature_insights(log_reg, test_data_processed, feature_names)
+
+
+        col_p, col_t = st.columns([1,1])
+        def segment_customer(row):
+            if row['MonthlyCharges'] > 80 and row['tenure'] < 12:  # Adjust thresholds
+                return "High Risk"
+            elif row['MonthlyCharges'] > 40 and row['tenure'] < 36:
+                return "Medium Risk"
+            else:
+                return "Low Risk"
+
+        def color_rows(row):
+            if row['Risk Segment'] == 'High Risk':
+                return ['background-color: red'] * len(row)
+            elif row['Risk Segment'] == 'Medium Risk':
+                return ['background-color: orange'] * len(row)
+            elif row['Risk Segment'] == 'Low Risk':
+                return ['background-color: green'] * len(row)
+            else:
+                return [''] * len(row)
+        with col_p:
+            st.markdown("<h3 class='centered-header'>Customer Segmentation Based on Tenure & MonthlyCharges</h3>", unsafe_allow_html=True)
+            test_data['Risk Segment'] = test_data.apply(segment_customer, axis=1)
+
+            segment_counts = test_data['Risk Segment'].value_counts().reset_index()
+            segment_counts.columns = ['Risk Segment', 'Count']
+
+            fig_segment = px.bar(segment_counts, x='Risk Segment', y='Count')
+            st.plotly_chart(fig_segment)
+
+        with col_t:
+            styled_table = test_data.style.apply(color_rows, axis=1)
+            st.markdown("<h3 class='centered-header'>Customer Data with Risk Segment Based on Tenure & MonthlyCharges</h3>", unsafe_allow_html=True)
+
+            st.dataframe(styled_table)
---
+++ customer_churn_training.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, classification_report
+from imblearn.over_sampling import SMOTE
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+import joblib
+
+
+
+# @st.cache_data
+def load_data():
+    path = 'WA_Fn-UseC_-Telco-Customer-Churn.csv'
+    data = pd.read_csv(path)
+    return data
+def process_senior_citizen(df):
+    try:
+        df['SeniorCitizen'] = df['SeniorCitizen'].map({0: 'No', 1: 'Yes'})
+    except Exception as e:
+        st.error(e)
+    return df
+
+
+# Process TotalCharges column
+def process_total_charges(df):
+    try:
+        df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+        df['TotalCharges'].fillna(0, inplace=True)
+    except Exception as e:
+        st.error(e)
+    return df
+# Handles missing values in a DataFrame.
+def na_value_handling(df):
+    try:
+        # Separate numerical and categorical columns
+        numerical_cols = df.select_dtypes(include=['number']).columns
+        categorical_cols = df.select_dtypes(include=['object']).columns
+
+        # Impute numerical columns with mean
+        numerical_imputer = SimpleImputer(strategy='mean')
+        df[numerical_cols] = numerical_imputer.fit_transform(df[numerical_cols])
+
+        # Impute categorical columns with most frequent value
+        categorical_imputer = SimpleImputer(strategy='most_frequent')
+        df[categorical_cols] = categorical_imputer.fit_transform(df[categorical_cols])
+
+        # Specific handling for 'SeniorCitizen' column
+        if 'SeniorCitizen' in df.columns:
+            # Assuming SeniorCitizen is numerical (binary 0 or 1)
+            df['SeniorCitizen'] = df['SeniorCitizen'].fillna(0)  # Filling NaN with 0 for SeniorCitizen
+
+    except Exception as e:
+        st.error(f"An error occurred during missing value handling: {e}")
+    return df
+
+# Encode categorical variables
+def encode_categorical(df):
+    try:
+        if 'customerID' in df.columns:
+            return pd.get_dummies(df.drop(columns=['customerID']), drop_first=True)
+        else:
+            return pd.get_dummies(df, drop_first=True)
+    except Exception as e:
+        st.error(e)
+    return df
+
+
+
+# Combine all transformations using pipe
+
+def process_data(df):
+    df_processed = (df
+                    .pipe(process_senior_citizen)
+                    .pipe(process_total_charges)
+                    .pipe(na_value_handling)
+                    .pipe(encode_categorical)
+                    )
+
+    print(df_processed.head())
+
+    # Define features and target variable
+    X = df_processed.drop(columns=['Churn_Yes'])
+    y = df_processed['Churn_Yes']
+
+    # Apply SMOTE to handle class imbalance
+    smote = SMOTE(sampling_strategy=0.5, random_state=42)
+    X_sampled, y_sampled = smote.fit_resample(X, y)
+
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X_sampled, y_sampled, test_size=0.2, random_state=42,
+                                                        stratify=y_sampled)
+
+    return X_train, X_test, y_train, y_test, X_test.columns
+def train_models(X_train, y_train, X_test, y_test):
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('classifier', LogisticRegression(random_state=42))
+    ])
+
+    parameters = [
+        {'classifier__penalty': ['l1', 'l2', 'elasticnet'], 'classifier__solver': ['liblinear', 'saga']},
+        {'classifier__C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+    ]
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    grid_search = GridSearchCV(pipeline, parameters, cv=cv, scoring='accuracy', n_jobs=-1)
+    grid_search.fit(X_train, y_train)
+    joblib.dump(grid_search.best_estimator_, "customer_churn_model.pkl")
+    return grid_search.best_estimator_
+
+def evaluate_models(model, X_test, y_test):
+    X_test_scaled = model.named_steps['scaler'].transform(X_test)
+    y_pred = model.named_steps['classifier'].predict(X_test_scaled)
+    y_prob = model.named_steps['classifier'].predict_proba(X_test_scaled)[:, 1]
+    print(classification_report(y_test, y_pred))
+    print(f'AUC-ROC: {roc_auc_score(y_test, y_prob):.4f}')
+
+
+if __name__ == "__main__":
+    data = load_data()
+    X_train, X_test, y_train, y_test, feature_names = process_data(data)
+    log_reg = train_models(X_train, y_train, X_test, y_test)
+    evaluate_models(log_reg, X_test, y_test)
+
+
+
